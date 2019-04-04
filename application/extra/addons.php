328c367c2d4a629ee505f49d210b62f64fbcd16f
shenqichao <kira_shen@qq.com>
2019-04-04 15:51:42 +0800

    add some addons

--- application/extra/addons.php
+++ application/extra/addons.php
@@ -4,8 +4,23 @@ return array (
   'autoload' => false,
   'hooks' => 
   array (
+    'user_sidenav_after' => 
+    array (
+      0 => 'cms',
+    ),
   ),
   'route' => 
   array (
+    '/blog$' => 'blog/index/index',
+    '/blog/p/[:id]' => 'blog/index/post',
+    '/blog/c/[:id]' => 'blog/index/category',
+    '/blog/archieve' => 'blog/index/archieve',
+    '/cms/$' => 'cms/index/index',
+    '/cms/a/[:diyname]' => 'cms/archives/index',
+    '/cms/t/[:name]' => 'cms/tags/index',
+    '/cms/p/[:diyname]' => 'cms/page/index',
+    '/cms/s' => 'cms/search/index',
+    '/cms/c/[:diyname]' => 'cms/channel/index',
+    '/cms/d/[:diyname]' => 'cms/diyform/index',
   ),
 );
